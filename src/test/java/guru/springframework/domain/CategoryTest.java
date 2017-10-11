b28713483a0d7b7a04b23488667400ab7f62b334
ttran9 <toddtran9@gmail.com>
2017-10-11 15:21:59 -0700

    creating a JUnit test for the Category object.

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+    @Test
+    public void getId() throws Exception {
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+
+}
