8f75db3d781e3fb24259ce29326de384a6f6c200
ttran9 <toddtran9@gmail.com>
2017-10-12 16:34:10 -0700

    Video 136. Making changes to allow user to create a recipe via a create form.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,20 +18,27 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-//    @RequestMapping("/recipe/show/{id}")
-//    public String showById(@PathVariable String id, Model model){
-//
-//        model.addAttribute("recipe", recipeService.findById(new Long(id)));
-//
-//        return "recipe/show";
-//    }
-
     @RequestMapping("/recipe/show/{id}")
-    public String showById1(@PathVariable String id, Model model){
+    public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
