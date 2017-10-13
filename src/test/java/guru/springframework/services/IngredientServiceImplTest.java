ed60468fc65d812c7e60f1f8b5f71fcbb7098602
ttran9 <toddtran9@gmail.com>
2017-10-13 15:29:05 -0700

    Videos 143 and 144. Tests for deleting an ingredient.

--- src/test/java/guru/springframework/services/IngredientServiceImplTest.java
+++ src/test/java/guru/springframework/services/IngredientServiceImplTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -106,6 +107,26 @@ public class IngredientServiceImplTest {
         assertEquals(Long.valueOf(3L), savedCommand.getId());
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, times(1)).save(any(Recipe.class));
-
     }
+
+    @Test
+    public void deleteById() throws Exception {
+        // given
+        Recipe recipe = new Recipe();
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(3L);
+        ingredient.setRecipe(recipe);
+        recipe.addIngredient(ingredient);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        // when
+        ingredientService.deleteById(1L, 3L);
+
+        // then
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(any(Recipe.class));
+    }
+
 }
